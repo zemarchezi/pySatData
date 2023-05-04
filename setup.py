2d85cfa9ed72a71e2ac137a15c573d9918bc4578
Jose <jpmarchezi@gmail.com>
2023-05-04 10:51:02 -0400

    add different download directory

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
                  ],
     keywords='satellite space data tools',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['requests',  'pytplot',
+    install_requires=['requests', 'pytplot-mpl-temp>=2.1.17',
                       'cdflib>=0.3.20', 'cdasws>=1.7.24', 'netCDF4',
                       'pywavelets', 'pyqtgraph==0.12.2', 'astropy', 'loguru',
                       'aacgmv2>=2.6.2', 'matplotlib', 'netCDF4',
