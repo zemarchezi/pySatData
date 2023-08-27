63b4fe355709ff582783914eb72157448b7e4c2e
Jose Paulo Marchezi <jpmarchezi@gmail.com>
2023-08-26 22:21:27 -0400

    update steput

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from codecs import open
 
 setup(
     name='pysatdata',
-    version='2.0.4',
+    version='2.0.5',
     description='Python Space Physics Satellite Data Analysis Toolkit',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
