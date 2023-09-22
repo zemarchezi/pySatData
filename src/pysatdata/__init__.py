a7209441fd33eeb2d0b772d3ac16e469d5dbc4bd
José Paulo Marchezi <jpmarchezi@gmail.com>
2023-09-22 15:09:40 -0400

    Update __init__.py

--- src/pysatdata/__init__.py
+++ src/pysatdata/__init__.py
@@ -1,15 +1,1 @@
-import os
 
-try:
-  VERSION_PATH = os.path.join(os.path.dirname(__file__), "VERSION.txt")
-
-  with open(VERSION_PATH, "r") as version_file:
-    VERSION = version_file.read().strip()
-except Exception:
-  import importlib.metadata
-  VERSION = importlib.metadata.version('pysatdata')
-  
-__version__ = VERSION
-__license__ = "MIT"
-__author__ = "José Paulo Marchezi"
-__status__ = "Development"
