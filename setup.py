25d96adc3431edf7e2a2a00a8655774d814ec41f
Jose Marchei <jpmarchezi@gmail.com>
2023-09-22 15:22:33 -0400

    New version

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 from codecs import open
 
 #
-with open('pyproject.toml', 'r') as f:
-    version = f.readlines()[2].split('\n')[0].split(' ')[-1].split('"')[1]
+# with open('pyproject.toml', 'r') as f:
+#     version = f.readlines()[2].split('\n')[0].split(' ')[-1].split('"')[1]
 
     
 setup(
     name='pysatdata',
-    version=version,
+    version='1.0.3',
     description='Python Space Physics Satellite Data Analysis Toolkit',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
