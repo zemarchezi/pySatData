dd52aaa77db4709b4c2ba671a8148ec7c5cec1eb
Jose Paulo Marchezi <jpmarchezi@gmail.com>
2024-01-29 16:19:15 -0500

    add dmsp data

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from codecs import open
     
 setup(
     name='pysatdata',
-    version='1.0.7',
+    version='1.0.8b',
     description='Python Space Physics Satellite Data Analysis Toolkit',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
