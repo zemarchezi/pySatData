48b481beb10ef939e150228cd27321952d5d69f6
Jose Paulo Marchezi <jpmarchezi@gmail.com>
2024-01-29 15:20:30 -0500

    separate the config file into folders

--- pysatdata/resources/config.py
+++ pysatdata/resources/config.py
@@ -1,10 +1,12 @@
 import json
 import pysatdata
 class openConfigFile():
-    def __init__(self):
+    def __init__(self, satellite):
+        self.satellite = satellite
         pathstring = pysatdata.__file__.replace("\\", "/")
         path = "/".join(pathstring.split("/")[:-1])
 
-        path = path + "/resources/config_file.json"
+        path = f"{path}/resources/{self.satellite}/config_file.json"
+        print("loading config file from: ", path)
         with open(path, 'r') as f:
             self.config_file_sat = json.load(f)
