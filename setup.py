25734981f6b82468da795a0a6a6727ee1a691ee2
Jose Marchezi <jpmarchezi@gmail.com>
2024-01-30 14:58:43 -0500

    update dmsp data

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from codecs import open
     
 setup(
     name='pysatdata',
-    version='1.0.10',
+    version='1.0.11',
     description='Python Space Physics Satellite Data Analysis Toolkit',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
